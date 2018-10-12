02bad0a50dfd552d0c3a602b70b073d6b844c221
BECCA LEE <REBECCA.KIRSTIN.LEE@GMAIL.COM>
2018-10-12 16:08:04 -0700

    Solves problem 4

--- app.js
+++ app.js
@@ -81,12 +81,21 @@ Test this function by hand in the console to get it working, and when you think 
 var testArray = [2, 3, 4]; //eslint-disable-line
 
 function sumArray(testArray) { //eslint-disable-line
-  // TODO: Write your code here
+  // create variables for numbers in string
+  var a = testArray[0];
+  var b = testArray[1];
+  var c = testArray[2];
+  //add numbers from string
+  var sum1 = sum(a,b)[0];
+  var sum2 = sum(sum1,c)[0];
+  //create message and return
+  var arrayMessage = `${testArray} was passed in as an array of numbers, and ${sum2} is their sum.`
+  return[sum2, arrayMessage];
 }
 
 // Here is the test for sumArray(); uncomment it to run it
 
-// testSumArray(testArray);
+testSumArray(testArray);
 
 // Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop. Don't forget to create a new branch for your work on the next question!
 
